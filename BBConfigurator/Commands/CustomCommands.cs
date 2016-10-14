5870e5e5c4f8a84f31afc87a93a56a0355e327ba
nicholas_pellizer <nicholasp21+github@gmail.com>
2016-10-14 11:29:18 +0200

    added serial primordial serial communication

---
+++ BBConfigurator/Commands/CustomCommands.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace BBConfigurator.Commands
+{
+    public  class CustomCommands
+    {
+        public static RoutedUICommand CloseWindow { get; set; }
+
+        public static void CloseWindowExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            ((Window)e.Parameter).Close();
+        }
+        public static void CanExecuteIfParameterIsNotNull(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            e.Handled = true;
+        }
+
+        static CustomCommands()
+        {
+            CloseWindow = new RoutedUICommand();
+            CloseWindow.InputGestures.Add(new KeyGesture(Key.F4, ModifierKeys.Alt));
+            
+        }
+
+       
+
+    }
+}
---
+++ BBConfigurator/Repository/BlackboxRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BBCommon;
+
+namespace BBConfigurator.Repository
+{
+    public class BlackboxRepository
+    {
+        public void InitWorker()
+        {
+            var configRepo = new ConfiguratorRepository();
+            
+            //_worker = new Worker(configRepo.LoadConfiguration());
+
+            //_workerThread = new Thread(_worker.Main);
+            //_workerThread.Name = "BlacBoxWorker";
+
+            //_workerThread.Start();
+
+            SerialPort port = new SerialPort(configRepo.LoadConfiguration().SerialPortName, 9600);
+            port.Open();
+            port.DataReceived += PortOnDataReceived;
+
+        }
+
+        private void PortOnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var port = (SerialPort) sender;
+
+            string s = port.ReadLine();
+
+            Console.WriteLine(s);
+
+        }
+
+        public void StopWorker()
+        {
+            //_worker.RequestStop();
+            //while (_workerThread.IsAlive) ;
+        }
+
+        public void ResetWorker()
+        {
+            //StopWorker();
+
+            //this.InitWorker();
+        }
+    }
+}
